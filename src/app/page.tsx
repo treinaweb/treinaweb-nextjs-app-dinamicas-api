c0e11ebad5b93636336eb4e5ab3245172c0e4239
wesley gado <wesleygado@gmail.com>
2025-02-03 16:59:33 -0300

    ajustes finais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import PostList from "../ui/components/PostList/PostList";
-import * as postRepository from '@/api/service/post.service';
+import * as postService from '@/api/service/post.service';
 
 export default async function Home() {
-  const posts = await postRepository.obterTodos();
+  const posts = await postService.obterTodos();
   return (
     <main>
       <PostList posts={posts}/>
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,10 +1,10 @@
-import * as postRepository from "@/api/service/post.service";
+import * as postService from "@/api/service/post.service";
 import PostBody from "@/ui/components/PostBody/PostBody";
 import { notFound } from "next/navigation";
 
 export default async function PostPage( { params } : { params: {slug: string}}) {
     const { slug } = await params;
-    const post = await postRepository.obterPorSlug(slug);
+    const post = await postService.obterPorSlug(slug);
 
     if(!post) {
         notFound();
